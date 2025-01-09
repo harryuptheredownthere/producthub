a289fcf32efcf04b0dbe3f8b1b2830d7592840c5
Harry Westaway <harrywestaway@Harrys-MacBook-Pro-2.local>
2025-01-10 10:49:02 +1100

    Initial commit

--- default.nix
+++ default.nix
@@ -13,10 +13,19 @@ pkgs.mkShell {
     python311Packages.python-dotenv
     python311Packages.gunicorn
     python311Packages.pip
+    python311Packages.setuptools
+    python311Packages.wheel
+    python311Packages.typing-extensions
+    python311Packages.cryptography  # For secrets module
+    python311Packages.python-dateutil  # For datetime handling
+    python311Packages.six  # Common dependency
+    python311Packages.certifi  # For requests
+    python311Packages.idna  # For requests
     gcc
   ];
 
   shellHook = ''
     export PYTHONPATH=$PWD/.nix-profile/lib/python3.11/site-packages:$PYTHONPATH
+    export PATH=$PWD/.nix-profile/bin:$PATH
   '';
 }
