78b1213e73fc6e8f2079477e5b34c2e3f441735e
Harry Westaway <harrywestaway@Harrys-MacBook-Pro-2.local>
2025-01-10 09:33:19 +1100

    Initial commit

--- default.nix
+++ default.nix
@@ -2,8 +2,17 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     python311Full
+    nodejs
+    # Python packages
     python3Packages.flask
+    python3Packages.flask-cors
     python3Packages.requests
-    # Add all your Python dependencies here
+    python3Packages.pandas
+    python3Packages.openpyxl
+    python3Packages.python-dotenv
+    python3Packages.gunicorn
+    # Development tools
+    python3Packages.pip
+    python3Packages.virtualenv
   ];
 }
